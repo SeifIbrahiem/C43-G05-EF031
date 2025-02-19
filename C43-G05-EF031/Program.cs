a5c895ac39232b32b24e2f3968c8902226237fd3
Greg Low <greg@gmail.com>
2025-02-19 22:07:52 +0200

    Entity frame work core

--- C43-G05-EF031/Program.cs
+++ C43-G05-EF031/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Entity frame work core 
+            //orm tool in dotnet
+            //orm <> object relation mapper 
+            //backend app <> 
+            //orm <App---Db>
+            //Importance with EFCORE
+            //Mapping
+            //code first : generate table per each class
+            //DB  first :  generate class per each table
+
+            //l2e(linq to entity)//crud
+            //create -- read -- update -- delete 
+
+            //TCP : Table per class
+            //TBH : Table per hierachy
+            //Tpcc: Table per concert class
+
+            //<><><><><><><><><><><><><><><><><><><><<><><><>//
+            //(ADO / EFCORE / DOPPER )
+            //the choice of tool depends on <the requirement of your projet>
+            //if you need light weight and fast  (dapper)
+            //if you need height level api and many features (ef core)
+            //if you need fine grained control overdata base operation (ado.net)
+            #endregion
         }
     }
 }
