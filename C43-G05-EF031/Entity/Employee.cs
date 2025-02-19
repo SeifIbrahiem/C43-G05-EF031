7063c36ff3dca1d42b1c9c7cd1ca03c2c946f951
Greg Low <greg@gmail.com>
2025-02-19 23:53:30 +0200

    BY DATA ANNOTATION

--- C43-G05-EF031/Entity/Employee.cs
+++ C43-G05-EF031/Entity/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +15,48 @@ namespace C43_G05_EF031.Entity
     // EF CORE SUPPORT 4 WAYS FOR MAPPING CLASS IN DB
     //1:BY CONVERTION ANY CLASS CONVERT TO TABLE
 
-    internal class Employee    //Table (Employee)
+    //internal class Employee    //Table (Employee)
+    //{
+    //    public int Id { get; set; } //PUBLIC numeric proberty named 'ID' or "Employee Id"
+    //    public string Name { get; set; } //reference type (required)
+
+    //    public int ?Age { get; set; } //nullable value type 
+
+    //    public string Email { get; set; } //value type : float salary required
+
+    //    public double ?salary { get; set; } //nvarchar (max)_optional
+
+    //    public DateTime Dateofcration { get; set; } //DateTime required
+    //}
+
+    //BY DATA ANNOTATION : set of attribute used for data validation
+    internal class Employee    
     {
-        public int Id { get; set; } //PUBLIC numeric proberty named 'ID' or "Employee Id"
-        public string Name { get; set; } //reference type (required)
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; } 
 
-        public int ?Age { get; set; } //nullable value type 
+        [Required]
+        [Column("EmpName",TypeName ="varchar")]
+        [MaxLength(50)]
+        [StringLength(50 , MinimumLength =10)]
+        public string ?Name { get; set; }
 
+        [Range(0,50)]
+        public int? Age { get; set; }
+        [EmailAddress]
         public string Email { get; set; } //value type : float salary required
+        [Phone]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        [Column(TypeName ="money")]
+        [DataType(DataType.Currency)]
+        public double? salary { get; set; }
 
-        public double ?salary { get; set; } //nvarchar (max)_optional
-
-        public DateTime Dateofcration { get; set; } //DateTime required
+        [NotMapped]
+        public double? totalsalary { get; set; }
+       
     }
+
+
 }
